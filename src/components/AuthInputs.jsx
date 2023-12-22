24b85a9189310fd970e84ec3724bf1da1dd64169
Maciej Dąbrowski <maciej.dabrowski299@gmail.com>
2023-12-22 17:31:11 +0100

    add few dynamic values in styled components and remove a warning in console log

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -16,19 +16,20 @@ const Label = styled.label`
 	font-weight: 700;
 	letter-spacing: 0.1em;
 	text-transform: uppercase;
-	color: ${props => (props.invalid ? '#f87171' : '#6b7280')};
+	color: ${props => (props.$invalid ? '#f87171' : '#6b7280')};
 `
 // In upper case I`m seting the color dynamic. To set this prop I must create a function, which take a prop form component and set true or false a value.
 const Input = styled.input`
 	width: 100%;
 	padding: 0.75rem 1rem;
 	line-height: 1.5;
-	background-color: #d1d5db;
-	color: #374151;
-	border: 1px solid transparent;
+	background-color: ${({ $invalid }) => ($invalid ? '#fed2d2' : '#d1d5db')};
+	color: ${({ $invalid }) => ($invalid ? '#ef4444' : '#374151')};
+	border: 1px solid ${({ $invalid }) => ($invalid ? '#fed2d2' : 'transparent')};
 	border-radius: 0.25rem;
 	box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `
+//for props which you pass to other packages in react you should use doloar sihn. like $$invalid
 
 export default function AuthInputs() {
 	const [enteredEmail, setEnteredEmail] = useState('')
@@ -54,10 +55,10 @@ export default function AuthInputs() {
 		<div id="auth-inputs">
 			<ControlContainer>
 				<p>
-					<Label invalid={emailNotValid}>Email</Label>
+					<Label $invalid={emailNotValid}>Email</Label>
 					<Input
 						type="email"
-						className={emailNotValid ? 'invalid' : undefined}
+						$invalid={emailNotValid}
 						onChange={event => handleInputChange('email', event.target.value)}
 					/>
 				</p>
